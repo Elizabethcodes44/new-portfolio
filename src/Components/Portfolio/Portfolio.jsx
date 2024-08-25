91348c3b926ab143892378e66aefb2b72f2c9879
Elizabeth <lizbeepmc@gmail.com>
2024-08-25 20:36:05 +0300

    added new apps

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -2,39 +2,59 @@ import "./Portfolio.scss";
 import sheleads from "/sheleads.jpg";
 import candy from "/candy.jpg";
 import foodApp from "/foodApp.jpg";
-import realestate from "/realestate.jpg";
+import budgetapp from "/budget.jpg";
+import travelapp from "/Travel1.jpg";
+import bookstore from "/bookstore.jpg";
 import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 import { useRef, useEffect } from "react";
 
 const projects = [
-  {
+  { 
     id: 1,
+    title: "Online Book Store",
+    img: bookstore,
+    desc: "Explore an online  collection of books across different  genres built with mock data, Easily search, wishlist, and add to your cart, all in one place. ",
+    url: "https://myonlinebookstore44.netlify.app/",
+
+  },
+  { 
+    id: 2,
+    title: "Travel App",
+    img: travelapp,
+    desc: "This front-end application integrates various RESTful APIs to display hotels, flights, and reviews, offering a comprehensive travel experience. With both dark and light mode options, it provides a user-friendly interface where you can effortlessly search for flights and hotels, book them, manage your bookings, and delete them as needed. ",
+    url: "https://mytravelapp44.netlify.app/",
+
+  },
+  { 
+    id: 3,
+    title: "My Budget Tracker",
+    img: budgetapp,
+    desc: "This is a simple front end app that allows users to Easily track their income, expenses, and overall budget in real-time. Add, edit, and delete transactions with just a few clicks.",
+    url: "https://elizabethsbudgetapp.netlify.app/",
+
+  },
+  {
+    id: 4,
     title: "Bump Tech App",
     img: sheleads,
     desc: "Bump Tech is a full-stack app designed for expecting mothers.",
-    url: "https://bumptech.netlify.app/",
+    url: "https://github.com/Elizabethcodes44/my-Final-Project",
   },
   {
-    id: 2,
+    id: 5,
     title: "E-commerce App",
     img: candy,
     desc: "This ongoing live user-friendly website allows users to easily browse through the collections, .",
     url: "https://candyglitterz.netlify.app/",
   },
   {
-    id: 3,
+    id: 6,
     title: "Recipe App",
     img: foodApp,
     desc: " An ongoing recipe app tailored for Nigerians residing in Istanbul, Turkey. ",
     url: "https://github.com/Elizabethcodes44/react-naija-recipe-App",
   },
-  {
-    id: 4,
-    title: "Real Estate App",
-    img: realestate,
-    desc: "This ongoing real estate app is set to revolutionize the property market with its comprehensive features and seamless user experience.",
-    url: "https://github.com/Elizabethcodes44/Gravano",
-  },
+  
 ];
 function Section({ project }) {
   const ref = useRef();
